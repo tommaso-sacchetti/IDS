fa94120f24742bcacd5c0cf8a9db41396fdfa44b
Tom <>
2023-04-21 17:01:29 +0200

    whitelist fixed

---
+++ check_file.py
@@ -0,0 +1,8 @@
+with open('whitelist.txt') as f:
+    seen = set()
+    for line in f:
+        line_lower = line.lower()
+        if line_lower in seen:
+            print(line)
+        else:
+            seen.add(line_lower)
